c209c3995008e40fd9f83f481fa26f7329d32463
Dario Hernandez <darehloza@gmail.com>
2024-02-19 18:54:14 -0600

    obtenemosel place buscar

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 const btnSearch = document.getElementById('btnBuscar')
 const inputCity = document.getElementById('searchCity')
 
-btnSearch.addEventListener('click', ()=> {
-  if(inputCity.value.trim().length > 0) {
-    console.log('input value => ', inputCity.value)
-    buscarCiudad(inputCity.value)
-  }
+btnSearch.addEventListener('click', () => {
+	if(inputCity.value.trim().length > 0) {
+		console.log('input value => ', inputCity.value)
+		buscarCiudad(inputCity.value)
+	}
 })
 
 const buscarCiudad = async (ciudad) => {
-  const url = `https://ai-weather-by-meteosource.p.rapidapi.com/find_places?text=${ciudad}language=en`;
-  const options = {
+	const url = `https://ai-weather-by-meteosource.p.rapidapi.com/find_places?text=${ciudad}&language=es`;
+const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'ab3a12a3f4mshd77e75215dbdc15p163778jsne4a99ace9bf7',
